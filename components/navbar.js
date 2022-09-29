a7cab2bae4dbcdd0d4637d74166faf000911c230
shyatup <quahshyawei@gmail.com>
2022-09-29 18:27:17 +0800

    edit navbar to remove active styling on roles

--- components/navbar.js
+++ components/navbar.js
@@ -73,7 +73,7 @@ app.component("navbar", {
     <div class="collapse navbar-collapse" id="navbarScroll">
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" :href=links.roleLink>Roles</a>
+          <a class="nav-link" aria-current="page" :href=links.roleLink>Roles</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" :href=links.skillLink>Skills</a>
