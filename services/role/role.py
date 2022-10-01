5a9440eaece82c9a68a7ee8ce7bfc48fcda047f3
jacky-teo <jackytjq@outlook.com>
2022-10-01 20:51:44 +0800

    Added Role fields from database update

--- services/role/role.py
+++ services/role/role.py
@@ -3,28 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 from os import environ
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/ljps'
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/ljps'
+# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 class Role(db.Model):
+    
     __tablename__ = 'roles'
 
     role_id = db.Column(db.Integer(), primary_key=True)
     role_name = db.Column(db.String(64), nullable=False)
     role_desc = db.Column(db.String(64), nullable=False)
     role_status = db.Column(db.Boolean(), nullable=False)
+    role_sector = db.Column(db.String(64), nullable=False)
+    role_track = db.Column(db.String(64), nullable=False)
 
-    def __init__(self, role_id, role_name, role_desc, role_status):
+    def __init__(self, role_id, role_name, role_desc, role_status, role_sector, role_track):
         self.role_id = role_id
         self.role_name = role_name
         self.role_desc = role_desc
         self.role_status = role_status
+        self.role_sector = role_sector
+        self.role_track = role_track
 
     def json(self):
-        return {"role_id": self.role_id, "role_name": self.role_name, "role_desc": self.role_desc, "role_status": self.role_status}
+        return {"role_id": self.role_id, "role_name": self.role_name, "role_desc": self.role_desc, "role_status": self.role_status, "role_sector": self.role_sector, "role_track": self.role_track}
 
 #AL-25 -- View all -- 
 @app.route("/roles")
@@ -52,34 +57,78 @@ def create_role():
     data = request.get_json()
     role_list = Role.query.all()
     role_names = []
-
     for role in role_list:
         role_names.append(role.role_name.lower())
 
     if data['role_name'].lower() in role_names:
-        print(data['role_name'])
         return jsonify(
             {
-                "code": 400,
+                "code": 409,
                 "data": {
                     "role_name": data['role_name'].lower()
                 },
                 "message": "This role already exists."
             }
         ), 400
-    if data['role_name'] == "" or data['role_desc'] == "":
+
+    if data['role_name'] == "":
         return jsonify(
             {
-                "code": 400,
+                "code": 409,
                 "data": {
                     "role_name": data['role_name']
                 },
-                "message": "Role name or role description cannot be empty."
+                "message": "Role name cannot be empty."
+            }
+        ), 400
+        
+    if data['role_desc'] == "":
+        return jsonify(
+            {
+                "code": 409,
+                "data": {
+                    "role_desc": data['role_desc']
+                },
+                "message": "Role description cannot be empty."
             }
         ), 400
 
+    if data['role_status'] == "":
+        return jsonify(
+            {
+                "code": 409,
+                "data": {
+                    "role_status": data['role_status']
+                },
+                "message": "Role status cannot be empty."
+            }
+        ), 400
+    
+    if data['role_sector'] == "":
+        return jsonify(
+            {
+                "code": 409,
+                "data": {
+                    "role_sector": data['role_sector']
+                },
+                "message": "Role sector cannot be empty."
+            }
+        ), 400
+    
+    if data['role_track'] == "":
+        return jsonify(
+            {
+                "code": 409,
+                "data": {
+                    "Role_track": data['role_track']
+                },
+                "message": "Role track cannot be empty."
+            }
+        ), 400
+
+
     role_id= role_list[-1].role_id +1
-    role = Role(role_id=role_id, role_name=data['role_name'].lower(), role_desc=data['role_desc'].lower(), role_status=data['role_status'])
+    role = Role(role_id=role_id, role_name=data['role_name'].lower(), role_desc=data['role_desc'].lower(), role_status=data['role_status'], role_sector=data['role_sector'].lower(), role_track=data['role_track'].lower())
 
     try:
         db.session.add(role)
@@ -103,18 +152,18 @@ def create_role():
     ), 201
 
 # AL-3 & AL-18 -- Update --
-@app.route("/roles/update/<int:role_id>", methods=['POST','GET'])
+@app.route("/roles/update/<int:role_id>", methods=['PUT','GET'])
 def update_role(role_id):
     pass
 
 
-#AL-17 --Delete--
-@app.route("/roles/delete/<int:role_id>", methods=['DELETE'])
-def delete_role(role_id):
+#AL-17 --hide_role--
+@app.route("/roles/delete/<int:role_id>", methods=['PUT'])
+def hide_role(role_id):
     pass
 
 
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000, debug=True)
+    app.run(port=5000, debug=True)
