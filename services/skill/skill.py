37ba15f66fe58373b09b87e2cee620b8c1eb7f18
shyatup <quahshyawei@gmail.com>
2022-10-01 20:56:45 +0800

    update logic to properly reflect acceptance criterias

--- services/skill/skill.py
+++ services/skill/skill.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import true
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/LJPS'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -28,57 +31,67 @@ class Skill(db.Model):
         return {"skill_id": self.skill_id, "skill_name": self.skill_name, "skill_category": self.skill_category, "skill_desc": self.skill_desc, "skill_status": self.skill_status}
 
 
-@app.route("/skill")
-def get_skills():
-    return jsonify({"skills": [skill.json() for skill in Skill.query.all()]})
+@app.route("/skill/<string:skill_id>")
+def get_skill(skill_id):
+    skill = Skill.query.filter_by(skill_id=skill_id).first()
+    if skill:
+        return jsonify(skill.json())
 
 
 @app.route("/skill/update/<string:skill_id>", methods=['GET', 'PUT'])
 def update_skill(skill_id):
     data = request.get_json()
     skill = Skill.query.filter_by(skill_id=skill_id).first()
+
     if skill:
-        # validation: check if no changes were made
         if data["skill_name"] == skill.skill_name and data["skill_category"] == skill.skill_category and data["skill_desc"] == skill.skill_desc and data["skill_status"] == skill.skill_status:
             return jsonify({
                 "code": 400,
                 "message": "No changes made."
             }), 400
-        else:
-            # validation: ensure that the skill name inserted is unique
-            if data["skill_name"] in [skill.skill_name for skill in Skill.query.all()]:
-                if data["skill_desc"] == skill.skill_desc:
-                    return jsonify({"code": 400, "message": "Skill name '{}' already exists.".format(data["skill_name"])}), 400
-
-
+        # validation: check if no changes were made
+        if data['skill_name'] != skill.skill_name:
             if data['skill_name'] != '':
                 skill.skill_name = data['skill_name']
             else:
                 return jsonify({"code": 400, "message": "Skill name cannot be empty."}), 400
+
+        if data['skill_category'] != skill.skill_category:
             if data['skill_category'] != '':
                 skill.skill_category = data['skill_category']
             else:
                 return jsonify({"code": 400, "message": "Skill category cannot be empty."}), 400
+
+        if data['skill_desc'] != skill.skill_desc:
             if data['skill_desc'] != '':
                 skill.skill_desc = data['skill_desc']
             else:
                 return jsonify({"code": 400, "message": "Skill description cannot be empty."}), 400
 
+        if data['skill_status'] != skill.skill_status:
             if data['skill_status'] != '':
                 skill.skill_status = data['skill_status']
             else:
                 return jsonify({"code": 400, "message": "Skill status cannot be empty."}), 400
-        db.session.commit()
+        try:
+            db.session.commit()
+            return jsonify({
+                "code": 200,
+                "data": skill.json(),
+                "message": "Skill updated."
+            })
+        except:
+            return jsonify({
+                "code": 500,
+                "message": "An error occurred while updating the skill."
+            }), 500
+
+    else:
         return jsonify({
-            "code": 200,
-            "data": skill.json(),
-            "message": "Skill updated."
-        })
-    return jsonify({
-        "code": 404,
-        "message": "Skill does not exist in database."
-    }), 404
+            "code": 404,
+            "message": "Skill does not exist in database."
+        }), 404
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5001, debug=True)
