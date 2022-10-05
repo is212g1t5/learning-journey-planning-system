5abbb89937adf200a6fe573fc97905aaad8ddc35
shyatup <quahshyawei@gmail.com>
2022-10-05 22:31:13 +0800

    add minor changes for consistency with other pages

--- users/hr/create_skill.js
+++ users/hr/create_skill.js
@@ -1,151 +1,135 @@
-const app = Vue.createApp({
-   data() {
-         return {
-            skillForm: {
-               name: '',
-               category: 'example',
-               description: 'testing only',
-               level: 1,
-               status: true, //0 - retired; 1 - active
-            },
-            confirmationMsg: '',
-            errorMsgs: {
-               name: '',
-               category: '',
-               description: '',
-            },
-            skill_api: {
-               create: "http://127.0.0.1:5001/skills/create",
-               getAll: "http://127.0.0.1:5001/skills",
-            },
-            existingSkills: [],
-         }
-   },
-   created() {
-      this.getAllSkillNames();
-      // this.getAllSkillCategories();
-   },
-   computed: {
-      isFormValid() {
-         return !this.skillForm.name.trim() 
-         || !this.skillForm.category.trim() 
-         || !this.skillForm.description.trim()
-         || Object.values(this.errorMsgs).some((error) => {return error !== ''});
+const create = Vue.createApp({
+  data() {
+    return {
+      skillForm: {
+        name: "",
+        category: "example",
+        description: "testing only",
+        level: 1,
+        status: true, //0 - retired; 1 - active
       },
-   },
-   watch: {
-      'skillForm.name'(newValue) {
-         if (newValue && newValue.trim().length > 0) {
-            
-            //check if skill name already exists
-            if (this.existingSkills.includes(newValue.toLowerCase())) {
-               this.errorMsgs.name = 'Skill already exists';
-
-            } else {
-               this.errorMsgs.name = '';
-            }
-
-         } else {
-            this.errorMsgs.name = 'Skill name cannot be empty';
-         }
-
+      confirmationMsg: "",
+      errorMsgs: {
+        name: "",
+        category: "",
+        description: "",
       },
-      //validate whether category is empty
-      'skillForm.category'(newValue) {
-         if (newValue && newValue.trim().length > 0) {
-            this.errorMsgs.category = '';
-
-         } else {
-            this.errorMsgs.category = 'Category cannot be empty';
-         }
+      skill_api: {
+        create: "http://127.0.0.1:5001/skills/create",
+        getAll: "http://127.0.0.1:5001/skills",
       },
-      //validate whether description is empty
-      'skillForm.description'(newValue) {
-         if (newValue && newValue.trim().length > 0) {
-            this.errorMsgs.description = '';
-
-         } else {
-            this.errorMsgs.description = 'Description cannot be empty';
-         }
+      existingSkills: [],
+    };
+  },
+  created() {
+    this.getAllSkillNames();
+    // this.getAllSkillCategories();
+  },
+  computed: {
+    isFormValid() {
+      return (
+        !this.skillForm.name.trim() ||
+        !this.skillForm.category.trim() ||
+        !this.skillForm.description.trim() ||
+        Object.values(this.errorMsgs).some((error) => {
+          return error !== "";
+        })
+      );
+    },
+  },
+  watch: {
+    "skillForm.name"(newValue) {
+      if (newValue && newValue.trim().length > 0) {
+        //check if skill name already exists
+        if (this.existingSkills.includes(newValue.toLowerCase())) {
+          this.errorMsgs.name = "Skill already exists";
+        } else {
+          this.errorMsgs.name = "";
+        }
+      } else {
+        this.errorMsgs.name = "Skill name cannot be empty";
       }
-   },
-   methods: {
-      //api call to retrieve all existing skill names
-      async getAllSkillNames() {
-         //tbc
-         this.existingSkills = ['skill1', 'skill2', 'skill3'];
+    },
+    //validate whether category is empty
+    "skillForm.category"(newValue) {
+      if (newValue && newValue.trim().length > 0) {
+        this.errorMsgs.category = "";
+      } else {
+        this.errorMsgs.category = "Category cannot be empty";
+      }
+    },
+    //validate whether description is empty
+    "skillForm.description"(newValue) {
+      if (newValue && newValue.trim().length > 0) {
+        this.errorMsgs.description = "";
+      } else {
+        this.errorMsgs.description = "Description cannot be empty";
+      }
+    },
+  },
+  methods: {
+    //api call to retrieve all existing skill names
+    async getAllSkillNames() {
+      //tbc
+      this.existingSkills = ["skill1", "skill2", "skill3"];
 
-         //call api to get all skill names
-         // try {
-         //    const res = await axios({
-         //       url: this.skill_api.getAll,
-         //    });
+      //call api to get all skill names
+      // try {
+      //    const res = await axios({
+      //       url: this.skill_api.getAll,
+      //    });
 
-         //    data = res.data.data;
-         //    console.log(data);
-         //    this.existingSkills = data;
+      //    data = res.data.data;
+      //    console.log(data);
+      //    this.existingSkills = data;
 
-         // } catch (err) {
-         //    // Handle Error Here
-         //    console.error(err);
-         // }
-      },
-      //api call to retrieve all existing skill categories
-      getAllSkillCategories(){
-         //tbc
-      },
-      //trigger confirmation popup before creating skill
-      confirmNewSkill() {
-         this.confirmationMsg = 'Are you sure you want to create this skill, '
-         + this.skillForm.name
-         + '?';
-         
-      },
-      //cancel skill creation
-      cancelNewSkill() {
-         this.confirmationMsg = '';
+      // } catch (err) {
+      //    // Handle Error Here
+      //    console.error(err);
+      // }
+    },
+    //api call to retrieve all existing skill categories
+    getAllSkillCategories() {
+      //tbc
+    },
+    //trigger confirmation popup before creating skill
+    confirmNewSkill() {
+      this.confirmationMsg =
+        "Are you sure you want to create this skill, " +
+        this.skillForm.name +
+        "?";
+    },
+    //cancel skill creation
+    cancelNewSkill() {
+      this.confirmationMsg = "";
+    },
+    //call role api to create new skill
+    async createNewSkill() {
+      //call api to create new skill
+      try {
+        const res = await axios({
+          method: "post",
+          url: this.skill_api.create,
+          data: {
+            skill_name: this.skillForm.name,
+            skill_category: this.skillForm.category,
+            skill_desc: this.skillForm.description,
+            skill_status: this.skillForm.status,
+          },
+        });
 
-      },
-      //call role api to create new skill
-      async createNewSkill() {
-         //call api to create new skill
-         try {
-            const res = await axios({
-               method: 'post',
-               url: this.skill_api.create,
-               data: {
-                  "skill_name": this.skillForm.name,
-                  "skill_category": this.skillForm.category,
-                  "skill_desc" : this.skillForm.description,
-                  "skill_status": this.skillForm.status
-               }
-            });
+        data = res.data.data;
+        console.log("New skill " + data.skill_name + " created successfully");
+        this.confirmationMsg = "";
 
-            data = res.data.data
-            console.log("New skill " + data.skill_name + " created successfully");
-            this.confirmationMsg = '';
-
-            //to be continued...
-            //redirect to created skill details page
-
-         } catch (err) {
-            // Handle Error Here
-            console.error(err);
-         }
-
-      },
-   }
+        //to be continued...
+        //redirect to created skill details page
+      } catch (err) {
+        // Handle Error Here
+        console.error(err);
+      }
+    },
+  },
 });
 
-// component templates
-app.component('sound-icon', {
-   data() {
-         return {
-            soundEmojis: ['🔇', '🔈', '🔉', '🔊']
-         }
-   },
-   props: ['level'],
-   template: `<span> {{soundEmojis[level]}} </span>` // TODO: add your template code here
-})
-
-app.mount("#create-skill");
+create.mount("#create-skill");
