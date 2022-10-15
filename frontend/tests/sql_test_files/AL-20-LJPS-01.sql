1e15f5a37ff3e16db2adb1651e4c1a457cb13c33
jacky-teo <jackytjq@outlook.com>
2022-10-15 19:33:19 +0800

    update sql test scripts

--- frontend/tests/sql_test_files/AL-20-LJPS-01.sql
+++ frontend/tests/sql_test_files/AL-20-LJPS-01.sql
@@ -1,4 +1,11 @@
+DROP TABLE IF EXISTS `skills_courses`;
+DROP TABLE IF EXISTS `lj_courses`;
+DROP TABLE IF EXISTS `registration`;
 DROP TABLE IF EXISTS `courses`;
+
+
+
+
 CREATE TABLE IF NOT EXISTS `courses` (
   `course_id` varchar(20) NOT NULL,
   `course_name` varchar(50) NOT NULL,
@@ -12,4 +19,31 @@ CREATE TABLE IF NOT EXISTS `courses` (
 INSERT INTO `courses` (`course_id`, `course_name`, `course_desc`, `course_status`, `course_type`, `course_category`) VALUES
 ('COR001', 'Systems Thinking and Design', 'This foundation module aims to introduce students to the fundamental concepts and underlying principles of systems thinking,', 1, 'Internal', 'Core'),
 ('FIN002', 'Risk and Compliance Reporting', 'Regulatory reporting is a requirement for businesses from highly regulated sectors to demonstrate compliance with the necessary regulatory provisions.', 1, 'External', 'Finance'),
-('FIN003', 'Business Continuity Planning', 'Business continuity planning is essential in any business to minimise loss when faced with potential threats and disruptions.', 0, 'External', 'Finance'),
+('FIN003', 'Business Continuity Planning', 'Business continuity planning is essential in any business to minimise loss when faced with potential threats and disruptions.', 0, 'External', 'Finance');
+
+DROP TABLE IF EXISTS `lj_courses`;
+CREATE TABLE IF NOT EXISTS `lj_courses` (
+  `learning_journey_id` int NOT NULL,
+  `course_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`learning_journey_id`,`course_id`),
+  KEY `lc_fk1` (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `skills_courses`;
+CREATE TABLE IF NOT EXISTS `skills_courses` (
+  `skill_id` int NOT NULL,
+  `course_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`skill_id`,`course_id`),
+  KEY `course_id` (`course_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `registration` (
+  `reg_id` int NOT NULL,
+  `course_id` varchar(20) NOT NULL,
+  `staff_id` int NOT NULL,
+  `reg_status` varchar(11) NOT NULL,
+  `completion_status` varchar(20) NOT NULL,
+  PRIMARY KEY (`reg_id`),
+  KEY `course_id` (`course_id`),
+  KEY `staff_id` (`staff_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
