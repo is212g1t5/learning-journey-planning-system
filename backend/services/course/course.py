a1c95808a7f88ce5ed28da3dbc4cedf2975435ee
jacky-teo <jackytjq@outlook.com>
2022-10-15 17:51:42 +0800

    Completed AL-20 along with Test cases

--- backend/services/course/course.py
+++ backend/services/course/course.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
+from flask_cors import CORS  # enable CORS
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/ljps'
@@ -172,6 +173,6 @@ def update_course(course_id):
     ), 404
 
 if __name__ == '__main__':
-    app.run( host='0.0.0.0', port=5002, debug=True)
+    app.run( host='0.0.0.0', port=5003, debug=True)
 
 # 
