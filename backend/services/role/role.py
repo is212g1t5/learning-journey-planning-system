d275cc7c23eda8083718858eab5f0aa776db779b
jacky-teo <jackytjq@outlook.com>
2022-10-19 14:44:19 +0800

    Changed Comments for Update Role

--- backend/services/role/role.py
+++ backend/services/role/role.py
@@ -177,14 +177,13 @@ def create_role():
     ), 201
 
 
-# AL-3 & AL-18 Update --
 @app.route("/roles/<int:role_id>")
 def get_role(role_id):
     role = Role.query.filter_by(role_id=role_id).first()
     if role:
         return jsonify(role.json())
 
-
+# AL-3 & AL-18 Update --
 @app.route("/roles/update/<int:role_id>", methods=['PUT', 'GET'])
 def update_role(role_id):
     data = request.get_json()
