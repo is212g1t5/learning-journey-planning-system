47afac4674fcf02bb85be165e37a333698b0704d
jacky-teo <jackytjq@outlook.com>
2022-11-02 01:47:37 +0800

    completed AL-65 frontend

--- frontend/api/learning_journey_details.js
+++ frontend/api/learning_journey_details.js
@@ -10,21 +10,46 @@ const display_learning_journey_details = Vue.createApp({
     },
     methods: {
         getLearningJourneyDetails() {
-            axios.get('http://localhost:5009/lj_courses/details/' + this.staff_id+'/'+this.lj_id)
+            axios.get('http://localhost:5004/learning_journeys/id/'+this.lj_id)
                 .then(response => {
-                    console.log(response.data.data)
-                    information = response.data.data
-                    for (res of information){
-                        this.learning_journey_details.push(res)
-                        console.log(res)
-                        this.lj_name = res.learning_journey_name
+                    
+                    information = response.data.data.learning_journey
+                        console.log('awaiting response...')
+                        console.log(information)
+                        var courses = information.courses
+                        this.lj_name = information['learning_journey_name']
+                        var course_list = Object.keys(courses)
+
+                        var skills = information.skills
+                        var skill_list = Object.keys(skills)
+                        results = []
+                        for (course of course_list) {
+                            var course_items = {}
+                            course_items['course_code'] = course
+                            course_items['status'] = courses[course]['status']
+                            course_items['skills'] = ''
+                            for (skill of skill_list) {
+                                var skill_info = skills[skill]
+
+                                var course_codes = skill_info['mapping']
+                                if (course_codes.includes(course)) {
+                                    console.log(course)
+                                    console.log(skill_info['skill_status'])
+                                    if (skill_info['skill_status']) {
+                                        console.log(skill_info['skill_name'])
+                                        course_items['skills'] += skill_info['skill_name'] + ', '
+                                    }
+                                }
+                            }
+                            results.push(course_items)
+                        this.learning_journey_details = results
                     }
-                    if (this.learning_journey_details.length == 0) {
-                        this.emptyText = "No learning journey details found"
-                    }
-                })
+
+                    
+                            })
                 .catch(error => {
                     console.log(error)
+                    this.emptyText = 'No Learning Journey Details Found'
                 })
         },
         deleteCourse(course_id) {
