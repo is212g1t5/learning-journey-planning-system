758eed133d5d94ea42fbd17a34587fe015e3ff5c
shyatup <quahshyawei@gmail.com>
2022-11-02 17:46:38 +0800

    fix minor changes

--- backend/tests/test_skill.py
+++ backend/tests/test_skill.py
@@ -1,16 +1,10 @@
 import unittest
-from urllib import response
 import flask_testing
-from learning_journey import app, db, LearningJourney
+import json
+import sys
+sys.path.insert(0, '../services/skill/')
 from skill import app, db, Skills
 
-# importing sys
-import sys
-
-# adding Folder_2 to the system path
-sys.path.insert(0, '../services/learning_journey/')
-
-\
 
 class TestApp(flask_testing.TestCase):
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
@@ -26,26 +20,6 @@ class TestApp(flask_testing.TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
-
-class TestDeleteLearningJourney(TestApp):
-    def test_delete_valid_learning_journey(self):
-        lj1 = LearningJourney(learning_journey_id=1, learning_journey_name="My First Learning Journey", staff_id=140882, role_id=3)
-        db.session.add(lj1)
-        db.session.commit()
-
-        response = self.client.delete("/learning_journeys/delete/1")
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json["data"], {
-            "learning_journey_id": 1
-        })
-    
-    def test_delete_invalid_learning_journey(self):
-
-        response = self.client.delete("/learning_journeys/delete/1")
-
-        self.assertEqual(response.status_code, 404)
-        self.assertEqual(response.json["message"], "Learning_Journey not found.")
 
 
 class TestSkill(TestApp):
@@ -305,6 +279,7 @@ class TestSkill(TestApp):
             'code': 400,
             'message': 'Skill is already active.'
         })
+
 
 if __name__ == '__main__':
     unittest.main()
